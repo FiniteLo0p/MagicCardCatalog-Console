ca883cc0157188b634268a96a30ac44e8017d308
Jason Smith <111402432+FiniteLo0p@users.noreply.github.com>
2022-08-22 17:29:34 -0500

    Created menus and class. Added / edited logic.

--- main.py
+++ main.py
@@ -2,13 +2,80 @@
 # August 21, 2022
 
 
-class Catalog:
-    """Represents a catalog instance."""
+class Main:
+    """Represents an instance of the application."""
     
     def __init__(self):
-        self.card_object = Card(name=None, type=None, collector_num=None, mana=None, 
-                         expansion=None)
+        # has a catalog
+        # has a card
+        pass
+        
+    def startup_menu(self):
+        """Menu dispalyed on startup."""
+        
+        print('')
+        print(' -- MAGIC CARD CATALOG --')
+        print('')
+        print('Press the number of the option below and press Enter:')
+        print('')
+        print('1. Create New Catalog')
+        print('2. Open Existing Catalog')
+        print('3. Exit')
+        print('')
+        
+        user_choice = str(input('Choice: '))
+        if user_choice == '1':
+            self.create_new_catalog()
+        elif user_choice == '2':
+            self.open_catalog()
+            self.open_catalog_menu()
+        elif user_choice == '3':
+            self.exit_app()
+        else:
+            print('')
+            print('!! Invalid number. Please try again. !!')
+            self.startup_menu()
+            
+    def new_catalog_menu(self):
+        """Menu disaplyed when a new catalog is created."""        
+        pass
     
+    def open_catalog_menu(self):
+        """Menu displayed when an existing catalog has been opened."""
+        
+        print('')
+        print('Press the number of the option below and press Enter:')
+        print('')
+        print('1. View Catalog')
+        print('2. Add Card')
+        print('3. Remove Card')
+        print('4. Save Catalog')
+        print('5. Go Back To Last Menu')
+        print('6. Exit')
+        print('')
+        
+        user_choice = str(input('Choice: '))
+        if user_choice == '1':
+            self.display_catalog()
+        elif user_choice == '2':
+            # call to catalog / card object
+            pass
+        elif user_choice == '3':
+            # call to catalog / card object
+            pass
+        elif user_choice == '4':
+            # may not be needed
+            pass
+        elif user_choice == '5':
+            self.startup_menu()
+        elif user_choice == '6':
+            # exit app
+            pass
+        else:
+            print('')
+            print('!! Invalid number. Please try again. !!')
+            self.open_catalog_menu()
+            
     def create_new_catalog(self):
         """This creates a new catalog (.txt) file."""
         pass
@@ -21,21 +88,43 @@ class Catalog:
         """Closes the currently open catalog file."""
         pass
     
-    def delete_catalog(self):
-        """Permanently deletes a catalog file."""
+    def display_catalog(self):
+        """"""
+        # sort in aplhabetical order?
+        # should user have option to view based on certain stats (type, name, 
+        # mana, etc)?
         pass
     
-    def save_catalog(self):
-        """Saves the currently open catalog file."""
-        pass
     
-    def remove_card(self):
+    # may not need:
+    # def delete_catalog(self):
+    #     """Permanently deletes a catalog file."""
+    #     pass
+    
+    # def save_catalog(self):
+    #     """Saves the currently open catalog file."""
+    #     pass
+    
+    def exit_app(self):
+        """"""
+        pass
+
+class Catalog:
+    """Represents a catalog instance."""
+    
+    def __init__(self):
+        # has a card
+        self._catalog = []  # [List of card objects]
+        self._card_object = Card(name=None, type=None, collector_num=None,
+                                mana=None, expansion=None)
+    
+    def remove_card(self, card):
         """Removes a card from the catalog file."""
-        pass
+        self._catalog.remove(card)
     
-    def add_card(self):
+    def add_card(self, card):
         """Adds a card to the catalog file."""
-        pass
+        self._catalog.append(card)
     
     
 class Card:
@@ -94,15 +183,8 @@ class Card:
 
 def main():
     """Mainloop."""
-    print('')
-    print(' -- MAGIC CARD CATALOG --')
-    print('')
-    print('Press the number of the option below:')
-    print('')
-    print('1. Create New Catalog')
-    print('2. Open Existing Catalog')
-    print('3. Exit')
-    print('')
+    app = Main()
+    app.startup_menu()
 
 
 if __name__ == "__main__":
